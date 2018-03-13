8ecfe74546ce1a748ff25fced65dfdafd4c4ace4
Devin Miller <devzmiller@gmail.com>
2018-03-13 14:32:53 -0700

    Modify csv parsing so the column titles are extracted and can be modified.

--- src/csv-parser.js
+++ src/csv-parser.js
@@ -1,31 +1,50 @@
-const parse = require('csv-parse')
+const parse = require('csv-parse/lib/sync')
 const fs = require('fs');
 
-const read = new Promise((resolve, reject) => {
-  fs.readFile('./cards.csv', 'utf8', (err, data) => {
-    if (err) throw err;
-    resolve(data)
-  })
-})
-
-read.then((data) => {
-  parser(data)
-})
-
-
-const parser = (data) => {
-  parse(data, {columns: true}, (err, output) => {
-    // console.log(output)
-    // output.map(card => {
-    //   card['tiiiiitle'] = card['Title']
-    //   delete card['Title']
-    //   return card['tiiiiitle']
-    // })
-    cardify(output)
+const read = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) throw err;
+      resolve(data)
+    })
   })
 }
 
+const importFile = async (filename) => {
+  let csvString = await read(filename)
+  let csvArray = parse(csvString)
+  let column_names = csvArray.shift()
+  console.log(column_names)
+  console.info(csvArray)
+  // update column names if needed so there's a Title and Type
+  const objects = []
+  csvArray.forEach((line) => {
+    const obj = {}
+    for (i = 0; i < line.length; i++) {
+      obj[column_names[i]] = line[i]
+    }
+    objects.push(obj)
+  })
+  console.log(objects)
+  console.log(cardify(objects))
+}
+
+importFile('./cards.csv')
+
+// const parser = (data) => {
+//   parse(data, {columns: true}, (err, output) => {
+//     // console.log(output)
+//     // output.map(card => {
+//     //   card['tiiiiitle'] = card['Title']
+//     //   delete card['Title']
+//     //   return card['tiiiiitle']
+//     // })
+//     cardify(output)
+//   })
+// }
+
 const cardify = (data) => {
+  const cards = []
   data.forEach(card => {
     let parsedCard = {
       workingTitle: null,
@@ -47,6 +66,7 @@ const cardify = (data) => {
         parsedCard.properties.push({name: prop, fieldId: null, content: card[prop]})
       }
     }
-    console.log(parsedCard)
+    cards.push(parsedCard)
   })
+  return cards
 }
